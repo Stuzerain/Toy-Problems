67d3d7f73beada37512152b460290ac42e090320
Stuzerain <shanford3@gmail.com>
2020-11-16 12:10:41 -0800

    Initial solution for jumpGame

--- jumpGame/jumpGame.js
+++ jumpGame/jumpGame.js
@@ -13,5 +13,24 @@
 // Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
 var canJump = function(nums) {
+  let previousIndex = null;
+  let currentIndex = 0;
 
+  while (previousIndex !== currentIndex) {
+    let jump = nums[currentIndex];
+    previousIndex = currentIndex;
+    currentIndex = currentIndex + jump;
+
+    if (currentIndex >= nums.length - 1) {
+      return true;
+    }
+  }
+  return false;
 };
+
+// canJump([2,3,1,1,4]) // true
+// canJump([3,2,1,0,4]) // false
+// canJump([1, 1, 1, 1, 1, 1, 1, 0]) // true
+// canJump([1, 1, 1, 1, 1, 1, 1, 0, 0]) // false
+// canJump([10, 0, 0, 0, 0, 0, 0, 0]) // true
+// canJump([0, 10, 10, 10, 10]) // false
